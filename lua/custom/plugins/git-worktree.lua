ca42e01132305012a71fd8b98809e5fdee1adaa5
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2025-03-20 13:04:04 +0100

    refactor(custom): use git worktree from polarmutex

--- lua/custom/plugins/git-worktree.lua
+++ lua/custom/plugins/git-worktree.lua
@@ -1,14 +1,14 @@
 return {
-  'ThePrimeagen/git-worktree.nvim',
+  'polarmutex/git-worktree.nvim',
+  version = '^2',
   dependencies = { 'telescope.nvim' },
   event = 'VimEnter',
   opts = {},
   keys = {
-    { '<leader>gw', "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", { silent = true, desc = 'Show Git worktrees' } },
+    { '<leader>gw', "<cmd>lua require('telescope').extensions.git_worktree.git_worktree()<CR>", { silent = true, desc = 'Show Git worktrees' } },
     { '<leader>gW', "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>", { silent = true, desc = 'Create Git worktree' } },
   },
-  config = function(_, opts)
-    require('git-worktree').setup(opts)
+  config = function()
     require('telescope').load_extension('git_worktree')
   end,
 }
