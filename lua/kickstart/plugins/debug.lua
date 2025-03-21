1167d0ad0f58a0365379e85a9b0cb857914491b1
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2025-03-21 18:00:25 +0100

    chore(debug): use of lettering keymaps
    I hate using F-keys

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -6,6 +6,24 @@
 -- be extended to other languages as well. That's why it's called
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 
+---@param config {type?:string, args?:string[]|fun():string[]?}
+local function get_args(config)
+  local args = type(config.args) == 'function' and (config.args() or {}) or config.args or {} --[[@as string[] | string ]]
+  local args_str = type(args) == 'table' and table.concat(args, ' ') or args --[[@as string]]
+
+  config = vim.deepcopy(config)
+  ---@cast args string[]
+  config.args = function()
+    local new_args = vim.fn.expand(vim.fn.input('Run with args: ', args_str)) --[[@as string]]
+    if config.type and config.type == 'java' then
+      ---@diagnostic disable-next-line: return-type-mismatch
+      return new_args
+    end
+    return require('dap.utils').splitstr(new_args)
+  end
+  return config
+end
+
 return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
@@ -25,63 +43,32 @@ return {
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
   },
+  -- stylua: ignore
   keys = {
-    -- Basic debugging keymaps, feel free to change to your liking!
-    {
-      '<F5>',
-      function()
-        require('dap').continue()
-      end,
-      desc = 'Debug: Start/Continue',
-    },
-    {
-      '<F1>',
-      function()
-        require('dap').step_into()
-      end,
-      desc = 'Debug: Step Into',
-    },
-    {
-      '<F2>',
-      function()
-        require('dap').step_over()
-      end,
-      desc = 'Debug: Step Over',
-    },
-    {
-      '<F3>',
-      function()
-        require('dap').step_out()
-      end,
-      desc = 'Debug: Step Out',
-    },
-    {
-      '<leader>b',
-      function()
-        require('dap').toggle_breakpoint()
-      end,
-      desc = 'Code Debug: Toggle Breakpoint',
-    },
-    {
-      '<leader>B',
-      function()
-        require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))
-      end,
-      desc = 'Code Debug: Set Breakpoint',
-    },
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    {
-      '<F7>',
-      function()
-        require('dapui').toggle()
-      end,
-      desc = 'Debug: See last session result.',
-    },
+    { '<leader>dB', function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = 'Breakpoint Condition' },
+    { '<leader>db', function() require('dap').toggle_breakpoint() end, desc = 'Toggle Breakpoint' },
+    { '<leader>dc', function() require('dap').continue() end, desc = 'Run/Continue' },
+    { '<leader>da', function() require('dap').continue({ before = get_args }) end, desc = 'Run with Args' },
+    { '<leader>dC', function() require('dap').run_to_cursor() end, desc = 'Run to Cursor' },
+    { '<leader>dg', function() require('dap').goto_() end, desc = 'Go to Line (No Execute)' },
+    { '<leader>di', function() require('dap').step_into() end, desc = 'Step Into' },
+    { '<leader>dj', function() require('dap').down() end, desc = 'Down' },
+    { '<leader>dk', function() require('dap').up() end, desc = 'Up' },
+    { '<leader>dl', function() require('dap').run_last() end, desc = 'Run Last' },
+    { '<leader>do', function() require('dap').step_out() end, desc = 'Step Out' },
+    { '<leader>dO', function() require('dap').step_over() end, desc = 'Step Over' },
+    { '<leader>dP', function() require('dap').pause() end, desc = 'Pause' },
+    { '<leader>dr', function() require('dap').repl.toggle() end, desc = 'Toggle REPL' },
+    { '<leader>ds', function() require('dap').session() end, desc = 'Session' },
+    { '<leader>dt', function() require('dap').terminate() end, desc = 'Terminate' },
+    { '<leader>dw', function() require('dap.ui.widgets').hover() end, desc = 'Widgets' },
   },
+  -- stylua: end
   config = function()
     local dap = require('dap')
     local dapui = require('dapui')
 
+    ---@diagnostic disable-next-line: missing-fields
     require('mason-nvim-dap').setup({
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
@@ -101,11 +88,13 @@ return {
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
+    ---@diagnostic disable-next-line: missing-fields
     dapui.setup({
       -- Set icons to characters that are more likely to work in every terminal.
       --    Feel free to remove or use ones that you like more! :)
       --    Don't feel like these are good choices.
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      ---@diagnostic disable-next-line: missing-fields
       controls = {
         icons = {
           pause = '⏸',
