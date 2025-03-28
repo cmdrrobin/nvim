f65a155c84cc99b2ba721d8d1b129224799a7dc4
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2025-03-28 11:40:30 +0100

    chore(plugins): remove nvim-cmp and use blink.cmp as default

--- lua/kickstart/plugins/blink.lua
+++ lua/kickstart/plugins/blink.lua
@@ -1,9 +1,8 @@
 return {
   {
     'saghen/blink.cmp',
-    -- optional: provides snippets for the snippet source
     dependencies = { 'rafamadriz/friendly-snippets' },
-
+    event = 'InsertEnter',
     -- use a release tag to download pre-built binaries
     version = '1.*',
 
