028f37a6aabf07baf5a1b2ef08f4bddf7699f7dd
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2025-03-28 11:55:45 +0100

    chore(blink): override provider settings
    Not sure if this is perfect, but need to try

--- lua/kickstart/plugins/blink.lua
+++ lua/kickstart/plugins/blink.lua
@@ -53,6 +53,40 @@ return {
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
+        providers = {
+          lsp = {
+            name = 'lsp',
+            enabled = true,
+            module = 'blink.cmp.sources.lsp',
+            score_offset = 90, -- the higher the number, the higher the priority
+          },
+          path = {
+            name = 'Path',
+            enabled = true,
+            module = 'blink.cmp.sources.path',
+            score_offset = 25,
+            -- When typing a path, I would get snippets and text in the
+            -- suggestions, I want those to show only if there are no path
+            -- suggestions
+            fallbacks = { 'snippets', 'buffer' },
+            opts = {
+              trailing_slash = false,
+              label_trailing_slash = true,
+              get_cwd = function(context)
+                return vim.fn.expand(('#%d:p:h'):format(context.bufnr))
+              end,
+              show_hidden_files_by_default = true,
+            },
+          },
+          buffer = {
+            name = 'Buffer',
+            enabled = true,
+            max_items = 3,
+            module = 'blink.cmp.sources.buffer',
+            min_keyword_length = 2,
+            score_offset = 15, -- the higher the number, the higher the priority
+          },
+        },
       },
 
       signature = {
