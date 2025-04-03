35fc044a6e4c1089f0455722b8e3926a42a58883
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2025-04-03 20:47:05 +0200

    chore(lsp): working version native lsp

--- init.lua
+++ init.lua
@@ -72,5 +72,8 @@ require('lazy').setup({
   },
 })
 
+-- [[ LSP configuration ]]
+require('kickstart.lsp')
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/extras/lang/go.lua
+++ lua/custom/extras/lang/go.lua
@@ -16,6 +16,13 @@ return {
     end,
   },
   {
+    'williamboman/mason-lspconfig.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { 'gopls' })
+    end,
+  },
+  {
     'stevearc/conform.nvim',
     optional = true,
     opts = {
