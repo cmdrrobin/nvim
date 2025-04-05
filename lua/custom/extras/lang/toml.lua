9a987eb31c14e305220a095670436c4141dc0c0f
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2025-04-05 11:21:57 +0200

    fix(lsp): ensure installed lsps

--- lua/custom/extras/lang/toml.lua
+++ lua/custom/extras/lang/toml.lua
@@ -7,6 +7,13 @@ return {
       })
     end,
   },
+  {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { 'taplo' })
+    end,
+  },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
