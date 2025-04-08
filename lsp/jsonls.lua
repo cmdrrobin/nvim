6cab99f00a07131051cc6d554eb8e41dcdd00abe
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2025-04-08 13:04:23 +0200

    feat(lsp): add jsonls

---
+++ lsp/jsonls.lua
@@ -0,0 +1,11 @@
+return {
+  cmd = { 'vscode-json-language-server', '--stdio' },
+  filetypes = { 'json', 'jsonc' },
+  init_options = {
+    provideFormatter = true,
+  },
+  root_markers = { '.git' },
+}
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
