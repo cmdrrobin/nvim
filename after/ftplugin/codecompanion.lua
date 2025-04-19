b81810db7a90fa79ae994ef87a6ce308f9d566c4
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2025-04-19 13:52:53 +0200

    chore(ft): update settings per filetype

---
+++ after/ftplugin/codecompanion.lua
@@ -0,0 +1,5 @@
+-- For some reason, this has to be scheduled
+vim.schedule(function()
+  vim.opt_local.colorcolumn = {}
+  vim.opt_local.showbreak = 'NONE'
+end)
--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,1 +1,3 @@
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
 vim.o.wrap = true
