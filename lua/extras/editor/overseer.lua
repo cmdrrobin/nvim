5226ec5e7021b74e3e695827f7dfd69886fb57b0
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2025-04-21 10:02:56 +0200

    feat(extras): add overseer plugin

---
+++ lua/extras/editor/overseer.lua
@@ -0,0 +1,74 @@
+---@module 'lazy'
+---@type LazySpec
+
+-- A task runner and job management plugin
+return {
+  {
+    'stevearc/overseer.nvim',
+    cmd = {
+      'OverseerOpen',
+      'OverseerClose',
+      'OverseerToggle',
+      'OverseerSaveBundle',
+      'OverseerLoadBundle',
+      'OverseerDeleteBundle',
+      'OverseerRunCmd',
+      'OverseerRun',
+      'OverseerInfo',
+      'OverseerBuild',
+      'OverseerQuickAction',
+      'OverseerTaskAction',
+      'OverseerClearCache',
+    },
+    opts = {
+      dap = false,
+      task_list = {
+        bindings = {
+          ['<C-h>'] = false,
+          ['<C-j>'] = false,
+          ['<C-k>'] = false,
+          ['<C-l>'] = false,
+        },
+      },
+      form = {
+        win_opts = {
+          winblend = 0,
+        },
+      },
+      confirm = {
+        win_opts = {
+          winblend = 0,
+        },
+      },
+      task_win = {
+        win_opts = {
+          winblend = 0,
+        },
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>ow", "<cmd>OverseerToggle<cr>",      desc = "Task list" },
+      { "<leader>oo", "<cmd>OverseerRun<cr>",         desc = "Run task" },
+      { "<leader>oq", "<cmd>OverseerQuickAction<cr>", desc = "Action recent task" },
+      { "<leader>oi", "<cmd>OverseerInfo<cr>",        desc = "Overseer Info" },
+      { "<leader>ob", "<cmd>OverseerBuild<cr>",       desc = "Task builder" },
+      { "<leader>ot", "<cmd>OverseerTaskAction<cr>",  desc = "Task action" },
+      { "<leader>oc", "<cmd>OverseerClearCache<cr>",  desc = "Clear cache" },
+    },
+  },
+  -- Update lualine to show copilot status
+  {
+    'nvim-lualine/lualine.nvim',
+    optional = true,
+    event = 'VeryLazy',
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, 2, {
+        'overseer',
+      })
+    end,
+  },
+}
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
