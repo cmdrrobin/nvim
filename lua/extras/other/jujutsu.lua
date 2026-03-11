e3abadc5439449008c3a28ed93203733f6800ad4
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2026-03-11 20:03:41 +0100

    chore(jujutsu): enable TS for jjdescription

--- lua/extras/other/jujutsu.lua
+++ lua/extras/other/jujutsu.lua
@@ -5,6 +5,13 @@ return {
     'martintrojer/jj-fugitive',
     cmd = 'J',
   },
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter',
+    config = function()
+      -- Register gitcommit parser for jjdescription filetype
+      vim.treesitter.language.register('gitcommit', 'jjdescription')
+    end,
+  },
   {
     'acarapetis/nvim-treesitter-jjconfig',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
