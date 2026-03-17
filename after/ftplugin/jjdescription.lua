3452c82a4dd51b2b5af42c80bf18ef4aed4129a6
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2026-03-17 14:35:18 +0100

    chore(ftplugin): set TS language for jujutsu

--- after/ftplugin/jjdescription.lua
+++ after/ftplugin/jjdescription.lua
@@ -1,3 +1,6 @@
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
 vim.o.wrap = true
+
+-- Use gitcommit TS with Jujutsu description filetyp
+vim.treesitter.language.register('gitcommit', 'jjdescription')
