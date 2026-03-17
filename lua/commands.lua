6cb70b834c9c881970779cbbfbd25cca664c75d2
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2026-03-17 20:26:04 +0100

    chore(commands): add custom vim.pack commands

--- lua/commands.lua
+++ lua/commands.lua
@@ -17,5 +17,53 @@ end, { desc = 'Toggle conform.nvim auto-formatting', nargs = 0 })
 vim.api.nvim_create_user_command('GoToFile', 'lua Snacks.picker.smart()', { desc = 'Open smart file picker', nargs = 0 })
 vim.api.nvim_create_user_command('GoToNotes', 'ObsidianSearch', { desc = 'Search Obsidian notes', nargs = 0 })
 
+local function complete_packages()
+  return vim
+    .iter(vim.pack.get())
+    :map(function(pack)
+      return pack.spec.name
+    end)
+    :totable()
+end
+
+vim.api.nvim_create_user_command('PackList', function()
+  vim.pack.update(nil, { offline = true })
+end, { desc = 'List Packages', nargs = 0 })
+
+vim.api.nvim_create_user_command('PackUpdate', function(info)
+  if #info.fargs ~= 0 then
+    vim.pack.update(info.fargs, { force = info.bang })
+  else
+    vim.pack.update(nil, { force = info.bang })
+  end
+end, {
+  desc = 'Update packages',
+  nargs = '*',
+  bang = true,
+  complete = complete_packages,
+})
+
+vim.api.nvim_create_user_command('PackSync', function(info)
+  if #info.fargs ~= 0 then
+    vim.pack.update(info.fargs, { target = 'lockfile', force = info.bang })
+  else
+    vim.pack.update(nil, { target = 'lockfile', force = info.bang })
+  end
+end, {
+  desc = 'Sync package(s)',
+  nargs = '*',
+  bang = true,
+  complete = complete_packages,
+})
+
+vim.api.nvim_create_user_command('PackDelete', function(info)
+  vim.pack.del(info.fargs, { force = info.bang })
+end, {
+  desc = 'Delete packages',
+  nargs = '+',
+  bang = true,
+  complete = complete_packages,
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
