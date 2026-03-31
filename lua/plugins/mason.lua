1ddc3503bec4fbd4865909c5e68ff0261e910902
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2026-03-31 16:54:18 +0200

    perf(mason): optimize loading

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -29,26 +29,31 @@ vim.pack.add({
   { src = 'https://github.com/mason-org/mason.nvim', data = { build = 'MasonUpdate' } },
 }, { confirm = false })
 
-require('mason').setup()
+vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter' }, {
+  once = true,
+  callback = function()
+    require('mason').setup()
 
-local mr = require('mason-registry')
-mr:on('package:install:success', function()
-  vim.defer_fn(function()
-    -- trigger FileType event to possibly load this newly installed LSP server or linter
-    vim.api.nvim_exec_autocmds('FileType', {
-      buffer = vim.api.nvim_get_current_buf(),
-    })
-  end, 100)
-end)
+    local mr = require('mason-registry')
+    mr:on('package:install:success', function()
+      vim.defer_fn(function()
+        -- trigger FileType event to possibly load this newly installed LSP server or linter
+        vim.api.nvim_exec_autocmds('FileType', {
+          buffer = vim.api.nvim_get_current_buf(),
+        })
+      end, 100)
+    end)
 
-mr.refresh(function()
-  for _, tool in ipairs(ensure_installed) do
-    local pkg = mr.get_package(tool)
-    if not pkg:is_installed() then
-      pkg:install()
-    end
-  end
-end)
+    mr.refresh(function()
+      for _, tool in ipairs(ensure_installed) do
+        local pkg = mr.get_package(tool)
+        if not pkg:is_installed() then
+          pkg:install()
+        end
+      end
+    end)
+  end,
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
