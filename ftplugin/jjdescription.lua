3c2047dd4c30745086e29f39bee6c1b7515cb80c
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2026-04-06 13:11:33 +0200

    fix(jj): remove custom setting

--- ftplugin/jjdescription.lua
+++ ftplugin/jjdescription.lua
@@ -1,6 +1,3 @@
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
 vim.o.wrap = true
-
--- Use gitcommit TS with Jujutsu description filetyp
-vim.treesitter.language.register('gitcommit', 'jjdescription')
