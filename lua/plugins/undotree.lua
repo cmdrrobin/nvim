03ebdceb9746a8c9afbf5bede5dcafaad82617bf
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2026-04-16 09:43:21 +0200

    chore(plugins): remove undotree package ref

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,12 +1,5 @@
 -- Undotree visualizes the undo history and makes it easy to browse and switch
 -- between different undo branches.
--- vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
---   once = true,
---   callback = function()
---     vim.pack.add({ 'https://github.com/mbbill/undotree' })
---     vim.keymap.set('n', '<leader>u', '<cmd>UndotreeToggle<CR>', { desc = 'Toggle Undotree' })
---   end,
--- })
 vim.cmd('packadd nvim.undotree')
 vim.keymap.set('n', '<leader>u', require('undotree').open)
 
