54a3a93926da87b677b4306763eb677c0c98f653
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2026-09-23 14:06:54 +0200

    feat(plugin): add tiny code actions and workspace diagnostics

--- plugin/tiny-code-action.lua
+++ plugin/tiny-code-action.lua
@@ -1,7 +1,11 @@
+vim.pack.add({
+  { src = 'https://github.com/rachartier/tiny-code-action.nvim' },
+}, { load = false })
+
 vim.api.nvim_create_autocmd('LspAttach', {
   once = true,
   callback = function()
-    vim.pack.add({ 'https://github.com/rachartier/tiny-code-action.nvim' })
+    vim.cmd.packadd('tiny-code-action.nvim')
 
     vim.keymap.set({ 'n', 'x' }, '<leader>ca', function()
       require('tiny-code-action').code_action()
--- plugin/workspace-diagnostics.lua
+++ plugin/workspace-diagnostics.lua
@@ -1,7 +1,10 @@
+vim.pack.add({
+  { src = 'https://github.com/artemave/workspace-diagnostics.nvim' },
+}, { load = false })
+
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function()
-    vim.pack.add({ 'https://github.com/artemave/workspace-diagnostics.nvim' })
-
+    vim.cmd.packadd('workspace-diagnostics.nvim')
     vim.lsp.config('*', {
       on_attach = function(client, bufnr)
         -- some clients support workspace diagnostics natively
